f22fb9149c74524d3ba31c66a11ecd7d1c81f975
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2025-01-19 09:00:06 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:appvotacionesg10/pages/home_page.dart';
+import 'package:appvotacionesg10/pages/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('Handling a background message: ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: "dev project",
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Configura el manejo de mensajes en segundo plano
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthWrapper(), // Controlador para mostrar login o página principal
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+}
